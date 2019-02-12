4c53b81b4849c0de1b6d138a86a28e425450ec93
Irakli Gozalishvili <rfobic@gmail.com>
2019-02-11 16:36:43 -0800

    Safari don't be like that please!!!

--- docs/lunet2.js
+++ docs/lunet2.js
@@ -1,6 +1,4 @@
 // SW forbids registering SW from other origins there for we just import
 // one from access point site.
 // WTF Safari!!!
-// importScripts("https://lunet.link/lunet/proxy.js")
-
-console.log("!!!!!!!!!!!!!!!!!")
+importScripts("https://lunet.link/lunet/proxy.js")
